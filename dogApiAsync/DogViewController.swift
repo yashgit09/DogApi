11d3198de6854f988d7957bcddcb9a7ce5a52731
Yash <yashkelkar09@gmail.com>
2022-07-13 22:22:17 -0400

    third commit-image is shown with name displayed

--- dogApiAsync/DogViewController.swift
+++ dogApiAsync/DogViewController.swift
@@ -7,6 +7,26 @@
 
 import UIKit
 
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
 
 class DogViewController: UIViewController {
 
@@ -21,7 +41,9 @@ class DogViewController: UIViewController {
 
     
            nameLbl.text = dog?.localized_name
-         
+           let imgUrl = "https://api.opendota.com" + (dog?.img)!
+           print(imgUrl)
+        dogImg.downloaded(from: imgUrl)
            
        }
 
